5fef08a8125d77a7f1cbffa9c218548c0f6c64b2
linem davton <linem.davton@gmail.com>
2024-07-01 22:03:24 +0200

    minor: test cases parameterized by algo and filename

--- tests/test_scheduling_algorithms.py
+++ tests/test_scheduling_algorithms.py
@@ -1,3 +1,4 @@
+from fastapi import applications
 import pytest
 import os
 import json
@@ -16,85 +17,91 @@ output_schema_file = os.path.join(script_dir, "..","src","output_schema.json")
 with open(output_schema_file) as f:
     output_schema = json.load(f)
 
-inputs = os.listdir(input_models_dir)
+input_files = os.listdir(input_models_dir)
+
+
+# List of algorithms to test
+algorithms = [ldf_single_node, edf_single_node, edf_multinode, ldf_multinode, ll_multinode]
+
+# Creating a product of filenames and algorithms for detailed parameterization
+test_cases = [(input_file, algo) for input_file in input_files for algo in algorithms]
 
 # Utility function to load models and run scheduling algorithm
-def load_and_schedule(filename):
+def load_and_schedule(filename, algo):
     model_path = os.path.join(input_models_dir, filename)
     with open(model_path) as f:
         model_data = json.load(f)
 
     application_model = model_data["application"]
     platform_model = model_data["platform"]
-    results = []
    
-    for algo in [ldf_single_node, edf_single_node]:
+    if algo in [ldf_single_node, edf_single_node]:
         result = algo(application_model)
-        results.append((result, application_model))
     
-    for algo in [edf_multinode, ldf_multinode, ll_multinode]:
+    elif algo in [edf_multinode, ldf_multinode, ll_multinode]:
         result = algo(application_model, platform_model)
-        results.append((result, application_model))
  
-    return results
+    return result, application_model
 
 
-@pytest.mark.parametrize("filename", inputs)
-def test_output_schema(filename):
+@pytest.mark.parametrize("filename,algorithm", test_cases)
+def test_output_schema(filename, algorithm):
     """Test that the generated schedule adheres to the schema """
 
-    for result, app_model in load_and_schedule(filename):
-        try:
-            jsonschema.validate(instance=result, schema=output_schema)
-            assert True
-        except jsonschema.exceptions.ValidationError as err:
-            assert False, f'Output does not match the output schema for {result["name"]}'
+    result, application_model = load_and_schedule(filename, algorithm)
+    try:
+        jsonschema.validate(instance=result, schema=output_schema)
+        assert True
+    except jsonschema.exceptions.ValidationError as err:
+        assert False, f'Output does not match the output schema for {result["name"]}'
         
 
-@pytest.mark.parametrize("filename", os.listdir(input_models_dir))
-def test_task_duration(filename):
+@pytest.mark.parametrize("filename, algorithm", test_cases)
+def test_task_duration(filename, algorithm):
     """Test that each task completes within its estimated duration."""
-    for result, app_model in load_and_schedule(filename):
-        for task in result["schedule"]:
-            start_time = task["start_time"]
-            end_time = task["end_time"]
-            task_id = task["task_id"]
-            wcet = next(
-                (t["wcet"] for t in app_model["tasks"] if t["id"] == task_id), None
-            )
-            assert end_time == start_time + wcet, f'Incorrect task duration calculation in {result["name"]}'
+    result, app_model = load_and_schedule(filename, algorithm)
+    for task in result["schedule"]:
+        start_time = task["start_time"]
+        end_time = task["end_time"]
+        task_id = task["task_id"]
+        wcet = next(
+            (t["wcet"] for t in app_model["tasks"] if t["id"] == task_id), None
+        )
+        assert end_time == start_time + wcet, f'Incorrect task duration calculation in {result["name"]}'
 
 
-@pytest.mark.parametrize("filename", os.listdir(input_models_dir))
-def test_task_deadline(filename):
+@pytest.mark.parametrize("filename, algorithm", test_cases)
+def test_task_deadline(filename,algorithm):
     """Test that each task respects its deadline."""
-    for result, app_model in load_and_schedule(filename):
-        for task in result["schedule"]:
-            end_time = task["end_time"]
-            task_id = task["task_id"]
-            deadline = next(
-                (t["deadline"] for t in app_model["tasks"] if t["id"] == task_id), None
-            )
-            assert end_time <= deadline, f'Task exceeds deadline in {result["name"]}'
+    result, app_model = load_and_schedule(filename, algorithm)
+    for task in result["schedule"]:
+        end_time = task["end_time"]
+        task_id = task["task_id"]
+        deadline = next(
+            (t["deadline"] for t in app_model["tasks"] if t["id"] == task_id), None
+        )
+        assert end_time <= deadline, f'Task exceeds deadline in {result["name"]}'
 
 
-@pytest.mark.parametrize("filename", os.listdir(input_models_dir))
-def test_task_dependencies(filename):
+@pytest.mark.parametrize("filename, algorithm", test_cases)
+def test_task_dependencies(filename, algorithm):
     """Test that each task respects the completion times of its predecessors."""
-    for result, app_model in load_and_schedule(filename):
-        for task in result["schedule"]:
-            start_time = task["start_time"]
-            task_id = task["task_id"]
-            predecessors = [
-                msg["sender"]
-                for msg in app_model["messages"]
-                if msg["receiver"] == task_id
+    result, app_model = load_and_schedule(filename, algorithm)
+    for task in result["schedule"]:
+        start_time = task["start_time"]
+        task_id = task["task_id"]
+        predecessors = [
+            msg["sender"]
+            for msg in app_model["messages"]
+            if msg["receiver"] == task_id
             ]
-            predecessors_end_times = [
-                t["end_time"]
-                for t in result["schedule"]
-                if t["task_id"] in predecessors
+        
+        predecessors_end_times = [
+            t["end_time"]
+            for t in result["schedule"]
+            if t["task_id"] in predecessors
             ]
-            assert start_time >= max(
+        
+        assert start_time >= max(
                 predecessors_end_times, default=0
-            ), f'Task starts before predecessor ends in {result["name"]}'
+        ), f'Task starts before predecessor ends in {result["name"]}'
